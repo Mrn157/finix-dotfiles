81510a731bf75e8063b2bcc2c19e40d9d2258aa3
Mrn157 <gamerzzz1510@gmail.com>
2026-05-31 22:08:47 +1200

    Added cliphist on niri startup + disabled hyprland

--- finix/configuration.nix
+++ finix/configuration.nix
@@ -134,7 +134,7 @@ in
 
     mangowc.enable = true;
 
-    hyprland.enable = true;
+    # hyprland.enable = true;
 
     spicetify =
       let
@@ -300,7 +300,7 @@ in
       nix-init nixd python3 yad eza rofi waydroid-helper waydroid steam prismlauncher w3m wget bluetui shadow openssh 
       ninja meson plocate gnumake mpv tmux p7zip steam-run libsm rofimoji chawan nh hyprlauncher zsh
       dualsensectl pcsx2 mgba reddit-tui openjdk17 emacs android-tools xdg-desktop-portal-gnome impala
-      cliphist openresolv nodejs obs-studio peazip
+      cliphist openresolv nodejs obs-studio peazip opengamepadui
 
       # Neovim Stuff
       neovim neovide docker-compose-language-service dockerfile-language-server emmet-language-server nixd nil ripgrep
